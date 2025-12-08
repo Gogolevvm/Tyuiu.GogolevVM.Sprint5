c6bb38cb7d1e647071ab51aa3cab530b57b14440
Всеволод Гоголев <Tyuiu.Gogolevvm@outlook.com>
2025-12-08 10:57:01 +0300

    Task4V20

--- Tyuiu.GogolevVM.Sprint5.Task4.V20.Lib/DataService.cs
+++ Tyuiu.GogolevVM.Sprint5.Task4.V20.Lib/DataService.cs
@@ -1,18 +1,32 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
 namespace Tyuiu.GogolevVM.Sprint5.Task4.V20.Lib
 {
     public class DataService : ISprint5Task4V20
     {
         public double LoadFromDataFile(string path)
         {
-            double x = 0;
-            string strX = File.ReadAllText(path);
-            x = double.Parse(strX);
+            // Читаем файл
+            string strX = File.ReadAllText(path, Encoding.UTF8);
 
+            // Убираем все возможные лишние символы
+            strX = strX.Trim('\uFEFF', '\u200B', ' ', '\t', '\n', '\r');
+
+            // Заменяем запятые на точки (на всякий случай)
+            strX = strX.Replace(',', '.');
+
+            // Преобразуем с инвариантной культурой
+            double x = double.Parse(strX, CultureInfo.InvariantCulture);
+
+            // Вычисляем результат
             double res = Math.Pow(((x * x) / Math.Sin(x)), 3);
-            res = Math.Round(res,3);
-            return res;
+            res = Math.Round(res, 3);
 
+            return res;
         }
     }
 }
