162d552b7917fc6e7b5f78cc274ebf905a7fb3f3
Всеволод Гоголев <Tyuiu.Gogolevvm@outlook.com>
2025-12-09 15:15:14 +0300

    Task5V20

--- Tyuiu.GogolevVM.Sprint5.Task5.V20.Lib/DataService.cs
+++ Tyuiu.GogolevVM.Sprint5.Task5.V20.Lib/DataService.cs
@@ -1,40 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 using System.IO;
+
 using tyuiu.cources.programming.interfaces.Sprint5;
 
-namespace Tyuiu.GogolevVM.Sprint5.Task5.V20.Lib
+namespace Tyuiu.KoevaSJ.Sprint5.Task5.V20.Lib
 {
     public class DataService : ISprint5Task5V20
     {
         public double LoadFromDataFile(string path)
         {
-            double sum = 0;
-            int count = 0;
 
-            // ТОЛЬКО ЧТЕНИЕ, никакой записи!
+            double res = 0;
             using (StreamReader reader = new StreamReader(path))
             {
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    line = line.Trim();
-                    if (string.IsNullOrEmpty(line))
-                        continue;
-
-                    // Пытаемся распарсить как целое число
-                    if (int.TryParse(line, out int number))
+                    double x = Convert.ToDouble(line);
+                    double sum = 0;
+                    if (x <= 10 && x >= -10)
                     {
-                        sum += number;
-                        count++;
+                        sum += x;
+                        res = sum / line.Length;
                     }
                 }
             }
+            return res;
 
-            if (count == 0)
-                return 0;
-
-            double average = sum / count;
-            return Math.Round(average, 3);
         }
     }
 }
